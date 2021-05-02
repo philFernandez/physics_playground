1dbc4c5e9a76500a763ccee4add74880a55ca9d3
philFernandez <norcal618@gmail.com>
2021-05-02 00:54:33 -0700

    doing things

--- src/js/app.js
+++ src/js/app.js
@@ -12,30 +12,35 @@ var box1;
 var ground;
 
 let boxes = [];
+const worldWidth = document.body.clientWidth;
+const worldHeight = document.body.clientHeight;
+console.log(`width : ${worldWidth}, height: ${worldHeight}`);
 
 function setup() {
-    createCanvas(900, 600);
+    createCanvas(worldWidth, worldHeight);
     engine = Engine.create();
     world = engine.world;
     Runner.run(engine);
     // this physics lib measures rectangles from center out
     // x = 450 would be the center of the rectangle
-    ground = Bodies.rectangle(450, height, width, 10, { isStatic: true });
+    ground = Bodies.rectangle(worldWidth / 2, height, width, 10, {
+        isStatic: true,
+    });
     Composite.add(world, ground);
 }
 
-// function mouseDragged() {
-//     boxes.push(new Box(mouseX, mouseY, 30, 30));
-// }
-function mousePressed() {
+function mouseDragged() {
     boxes.push(new Box(mouseX, mouseY, 30, 30));
 }
+// function mousePressed() {
+//     boxes.push(new Box(mouseX, mouseY, 30, 30));
+// }
 
 function draw() {
-    background("white");
+    background("lightgray");
     boxes.forEach((box) => box.show());
     rectMode(CENTER);
     fill("lightblue");
     stroke("blue");
-    rect(450, height, width, 10);
+    rect(worldWidth / 2, height, width, 10);
 }
