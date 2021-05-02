511c3aed43569c530ed8916511a099440584ca55
philFernandez <norcal618@gmail.com>
2021-05-01 23:18:10 -0700

    add ground that does nothing but stop

--- src/js/app.js
+++ src/js/app.js
@@ -15,11 +15,13 @@ function setup() {
     engine = Engine.create();
     world = engine.world;
     box1 = Bodies.rectangle(200, 100, 80, 80);
+    ground = Bodies.rectangle(0, 500, 900, 60, { isStatic: true });
     Runner.run(engine);
-    Composite.add(world, box1);
+    Composite.add(world, [box1, ground]);
 }
 
 function draw() {
     background(51);
     rect(box1.position.x, box1.position.y, 80, 80);
+    rect(ground.position.x, ground.position.y, 900, 60);
 }
